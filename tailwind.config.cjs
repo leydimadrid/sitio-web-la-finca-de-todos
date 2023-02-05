7af095efd540f9446cf2fc8b79fc349ebf86bb70
Leydi <leydi.madrid6@gmail.com>
2023-02-05 06:47:45 -0500

    Sitio Actualizado

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.jsx"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
