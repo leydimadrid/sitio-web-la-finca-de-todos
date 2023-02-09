f0cc525f70a853f6eff2efdf95f917d7fe505980
Leydi <leydi.madrid6@gmail.com>
2023-02-09 08:11:47 -0500

    Nuevos avances

--- src/components/Accesibilidad.jsx
+++ src/components/Accesibilidad.jsx
@@ -1,7 +1,7 @@
 export const Accesibilidad = ({ imagen, titulo, alt }) => {
   return (
     <section className="mt-6 max-w-sm p-4">
-    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+    <div className="max-w-sm rounded-lg overflow-hidden shadow-lg bg-white">
       <img className="w-full" src={imagen} alt={alt} />
       <div className="px-2 py-4 text-center">
         <h1 className="font-bold text-lg text-blue-900 leading-normal">{titulo}</h1>
