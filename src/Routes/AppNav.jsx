48f6cf261b0769bd0efe432dde3eb51f2a5fec63
Leydi <leydi.madrid6@gmail.com>
2023-03-08 16:44:08 -0500

    Correcciones listas

--- src/Routes/AppNav.jsx
+++ src/Routes/AppNav.jsx
@@ -1,26 +1,25 @@
-import { Routes, Route } from "react-router-dom";
-import { Inicio } from "../pages/Inicio";
-import { Nosotros } from "../pages/Nosotros";
-import { LaFinca } from "../pages/LaFinca";
-import { FinDeSemana } from "../pages/FinDeSemana";
-import { TalleresPedagogicos } from "../pages/TalleresPedagogicos";
-import { Ubicacion } from "../pages/Ubicacion";
-import { Donaciones } from "../pages/Donaciones";
-import { Contacto } from "../pages/Contacto";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Inicio } from '../pages/Inicio';
+import { Nosotros } from '../pages/Nosotros';
+import { LaFinca } from '../pages/LaFinca';
+import { FinDeSemana } from '../pages/FinDeSemana';
+import { TalleresPedagogicos } from '../pages/TalleresPedagogicos';
+import { Ubicacion } from '../pages/Ubicacion';
+import { Donaciones } from '../pages/Donaciones';
+import { Contacto } from '../pages/Contacto';
 
 export const AppNav = () => {
-
-  return (  
-    <Routes>
-        <Route path="/*" element={<Inicio/>} />
-        <Route path="/nosotros" element={<Nosotros/>} />
-        <Route path="/lafinca" element={<LaFinca/>} />
-        <Route path="/findesemana" element={<FinDeSemana/>} />
-        <Route path="/talleres" element={<TalleresPedagogicos/>} />
-        <Route path="/ubicacion" element={<Ubicacion/>} />
-        <Route path="/donaciones" element={<Donaciones/>} />
-        <Route path="/contacto" element={<Contacto/>} />
-      </Routes>
-
-  )
-}
+	return (
+		<Routes>
+      <Route path='/' element={<Navigate to="/inicio"/>} />
+			<Route path='/inicio'element={<Inicio />} />
+			<Route path='/nosotros' element={<Nosotros />} />
+			<Route path='/lafinca' element={<LaFinca />} />
+			<Route path='/findesemana' element={<FinDeSemana />} />
+			<Route path='/talleres' element={<TalleresPedagogicos />} />
+			<Route path='/ubicacion' element={<Ubicacion />} />
+			<Route path='/donaciones' element={<Donaciones />} />
+			<Route path='/contacto' element={<Contacto />} />
+		</Routes>
+	);
+};
