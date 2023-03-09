c93de403954549f0cf70a491c6a92bbf2cf6e4b7
Leydi <leydi.madrid6@gmail.com>
2023-03-09 14:59:05 -0500

    Se organiza videos

--- src/components/VideoInicio.jsx
+++ src/components/VideoInicio.jsx
@@ -4,8 +4,9 @@ export const VideoInicio = () => {
 			<section className='pt-10 md:px-40 px-5 md:pt-6'>
 				<div className='flex justify-center md:mb-4 mb-4 shadow-md'>
 					<iframe
+						className='md:max-w-full lg:max-w-full sm:max-w-full xl:max-w-full max-w-xs max-h-44 md:max-h-96 lg:max-h-96 sm:max-h-96 xl:max-h-full'
 						width='1490'
-						height='839'
+						height='840'
 						src='https://www.youtube.com/embed/WHID4wFqkpc'
 						title='YouTube video player'
 						allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
