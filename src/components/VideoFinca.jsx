11e591453959b2d54cc297753dc60f56c628f003
Leydi <leydi.madrid6@gmail.com>
2023-03-13 11:00:59 -0500

    Se agrega video finca en 3D

--- src/components/VideoFinca.jsx
+++ src/components/VideoFinca.jsx
@@ -7,7 +7,7 @@ export const VideoFinca = () => {
 				className="md:max-w-full lg:max-w-full sm:max-w-full xl:max-w-full max-w-xs max-h-40 md:max-h-80 lg:max-h-80 sm:max-h-80 xl:max-h-full"
 					width='1490'
 					height='839'
-					src='https://www.youtube.com/embed/-F3TYRFT7DE'
+					src='https://www.youtube.com/embed/GtDCd-yA6oE'
 					title='YouTube video player'
 					allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
 					allowfullscreen
